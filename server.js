33a38b581bd54a19c55947c9777692ef8448c592
yi <yungpxxp@gmail.com>
2024-02-21 17:16:58 +0600

    Updating

--- server.js
+++ server.js
@@ -1,5 +1,69 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
-const ejs = require('ejs');
-const mongosh = require('mongosh');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path'); // Import the path module
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'front','views'));
+
+//MongoDB
+mongoose.connect('mongodb://localhost:27017/portfolio', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    username: String,
+    phone: String,
+    password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+app.get('/', (req, res) => {
+    res.render('index'); // Render the index.ejs file
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = new User({
+            name: req.body.name,
+            username: req.body.username,
+            phone: req.body.phone,
+            password: hashedPassword,
+        });
+        await user.save();
+        res.send('Registration successful');
+    } catch (error) {
+        res.status(500).send('Error registering user');
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(400).send('Invalid password');
+        }
+        res.send('Login successful');
+    } catch (error) {
+        res.status(500).send('Error logging in');
+    }
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
